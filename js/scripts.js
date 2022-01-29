a805cba397078352709e1ad0c149e58523f64350
Sushil-azaas <sushilsunuwar@azaas.com>
2022-01-30 00:51:59 +0800

    rsvp spreadsheet

--- js/scripts.js
+++ js/scripts.js
@@ -166,7 +166,7 @@ $(document).ready(function () {
   $(function () {
     $(window).scroll(function () {
       var scroll = $(window).scrollTop();
-
+console.log(scroll)
       if (scroll >= 20) {
         $("section.navigation").addClass("fixed");
         $("header").css({
@@ -343,19 +343,19 @@ $(document).ready(function () {
     }
   });
 
-  const scriptURL =
-    "https://script.google.com/macros/s/AKfycbxGXt2Ds7l0BCvK7exKZuAsYvQOzguiOaaF5-V5ka08gtADynG9P-6NufQKts8uwr4Gvg/exec";
-  const form = document.forms["google-sheet"];
+  // const scriptURL =
+  //   "https://script.google.com/macros/s/AKfycbxGXt2Ds7l0BCvK7exKZuAsYvQOzguiOaaF5-V5ka08gtADynG9P-6NufQKts8uwr4Gvg/exec";
+  // const form = document.forms["google-sheet"];
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
-      .then(
-        (response) => console.log(response),
-        alert("Thanks for Contacting us..! We Will Contact You Soon...")
-      )
-      .catch((error) => console.error("Error!", error.message));
-  });
+  // form.addEventListener("submit", (e) => {
+  //   e.preventDefault();
+  //   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+  //     .then(
+  //       (response) => console.log(response),
+  //       alert("Thanks for Contacting us..! We Will Contact You Soon...")
+  //     )
+  //     .catch((error) => console.error("Error!", error.message));
+  // });
 });
 
 /********************** Extras **********************/
